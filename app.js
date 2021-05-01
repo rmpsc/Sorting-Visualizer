2d024098a381c49c8f424728cf19fd109c688ef9
Romel Pascua <romelpascua14@gmail.com>
2021-05-01 16:54:17 -0700

    clean selectionSort + start bubbleSort

--- app.js
+++ app.js
@@ -53,21 +53,25 @@ function resetArray() {
 function sort() {
 
     if (currentAlgo === "") {
-        console.log("no algo selected");
+        alert("Please select an algorithm.");
     } 
     
     else if (currentAlgo === "bubble") {
-        bubbleSort(arr);
+        bubbleSort(300);
+    }
+
+    else if (currentAlgo === "selection") {
+        selectionSort(300);
     }
 
     console.log(`sorting with ${currentAlgo}`)
 }
 
-function bubbleSort(arr) {
+function bubbleSortNorm(arr) {
     for (var i = 0; i < arr.length; i++) {
 
         // Last i elements are already in place  
-        for (var j = 0; j < ( arr.length -i -1 ); j++){
+        for (var j = 0; j < ( arr.length -i -1); j++){
             
             // Checking if the item at present iteration 
             // is greater than the next iteration
@@ -83,71 +87,90 @@ function bubbleSort(arr) {
     console.log(arr);
 }
 
-async function selectionSort(delay = 300) {
+async function bubbleSort(delay) {
     let bars = document.querySelectorAll(".bar");
-// Assign 0 to min_idx
-var min_idx = 0;
-for (var i = 0; i < bars.length; i += 1) {
 
-	// Assign i to min_idx
-	min_idx = i;
+    for (var i = 0; i < bars.length; i++) {
+        bars[i].style.backgroundColor = "white";
 
-	// Provide darkblue color to the ith bar
-	bars[i].style.backgroundColor = "darkblue";
-	for (var j = i + 1; j < bars.length; j += 1) {
+        for (var j = 0; j < bars.length -i -1; j++) {
+            bars[j].style.backgroundColor = "white";
 
-	// Provide red color to the jth bar
-	bars[j].style.backgroundColor = "red";
-		
-	// To pause the execution of code for 300 milliseconds
-	await new Promise((resolve) =>
-		setTimeout(() => {
-		resolve();
-		}, 300)
-	);
+             // store current j value
+             var curr_j = parseInt(bars[j].childNodes[0].innerHTML);
 
-	// To store the integer value of jth bar to var1
-	var val1 = parseInt(bars[j].childNodes[0].innerHTML);
+             // stores minimum value so far
+             var next_j = parseInt(bars[j + 1].childNodes[0].innerHTML);
 
-	// To store the integer value of (min_idx)th bar to var2
-	var val2 = parseInt(bars[min_idx].childNodes[0].innerHTML);
-		
-	// Compare val1 & val2
-	if (val1 < val2) {
-		if (min_idx !== i) {
-
-		// Provide salmon color to the (min-idx)th bar
-		bars[min_idx].style.backgroundColor = "salmon";
-		}
-		min_idx = j;
-	} else {
-
-		// Provide salmon color to the jth bar
-		bars[j].style.backgroundColor = "salmon";
-	}
-	}
-
-	// To swap ith and (min_idx)th bar
-	var temp1 = bars[min_idx].style.height;
-	var temp2 = bars[min_idx].childNodes[0].innerText;
-	bars[min_idx].style.height = bars[i].style.height;
-	bars[i].style.height = temp1;
-	bars[min_idx].childNodes[0].innerText = bars[i].childNodes[0].innerText;
-	bars[i].childNodes[0].innerText = temp2;
-	
-	// To pause the execution of code for 300 milliseconds
-	await new Promise((resolve) =>
-	setTimeout(() => {
-		resolve();
-	}, 300)
-	);
-
-	// Provide salmon color to the (min-idx)th bar
-	bars[min_idx].style.backgroundColor = "salmon";
-
-	// Provide lightgreen color to the ith bar
-	bars[i].style.backgroundColor = " rgb(49, 226, 13)";
+        }
+    }
 }
+
+async function selectionSort(delay) {
+    let bars = document.querySelectorAll(".bar");
+
+    var min_idx = 0;
+
+    for (var i = 0; i < bars.length; i++) {
+        // assign i to min_idx
+        min_idx = i;
+        // current index is red
+        bars[i].style.backgroundColor = "red";
+
+        for (var j = i + 1; j < bars.length; j++) {
+
+            // jth bar is white
+            bars[j].style.backgroundColor = "white";
+                
+            // pauses code for duration of delay
+            await new Promise((resolve) =>
+                setTimeout(() => {
+                resolve();
+                }, delay)
+            );
+
+            // store current j value
+            var current = parseInt(bars[j].childNodes[0].innerHTML);
+
+            // stores minimum value so far
+            var min = parseInt(bars[min_idx].childNodes[0].innerHTML);
+                
+            if (current < min) {
+                // if min index isn't the current i index
+                if (min_idx !== i) {
+                    // change color from red back to salmon, we've found a smaller int
+                    bars[min_idx].style.backgroundColor = "salmon";
+                }
+                // set new min_idx
+                min_idx = j;
+            } else {
+
+                // change color from white back to salmon
+                bars[j].style.backgroundColor = "salmon";
+            }
+        }
+
+        // To swap ith and (min_idx)th bar
+        var temp1 = bars[min_idx].style.height;
+        var temp2 = bars[min_idx].childNodes[0].innerText;
+        bars[min_idx].style.height = bars[i].style.height;
+        bars[i].style.height = temp1;
+        bars[min_idx].childNodes[0].innerText = bars[i].childNodes[0].innerText;
+        bars[i].childNodes[0].innerText = temp2;
+        
+        // To pause the execution of code for 300 milliseconds
+        await new Promise((resolve) =>
+        setTimeout(() => {
+            resolve();
+        }, delay)
+        );
+
+        // reset (min_idx)th bar to salmon
+        bars[min_idx].style.backgroundColor = "salmon";
+
+        // change in place bar to light green
+        bars[i].style.backgroundColor = " rgb(49, 226, 13)";
+    }
 }
 function main() {
 
@@ -159,7 +182,7 @@ function main() {
 
     quick_div.addEventListener("click", () => selectAlgo("quick"));
 
-    selection_div.addEventListener("click", () => selectionSort());
+    selection_div.addEventListener("click", () => selectAlgo("selection"));
 
     newArray_div.addEventListener("click", () => resetArray());
 
